4ee2ecbcddc9f97af8d5d5c62133cae633faf7c3
Gyulai Gergő Zsolt <144451652+gyulaig@users.noreply.github.com>
2023-10-09 13:15:08 +0200

    asdasdasd

---
+++ feladatok/feladat_009.py
@@ -0,0 +1,12 @@
+szam = int(input("Kérek egy számot"))
+print(f"A megadott számod: {szam}")
+
+# ... -1, 0, 1, 2, ...8, 9
+#if szam <10
+#   print(f"A szám kor isebb mint 10!")
+
+# ... -1, 0, 1, 2, ...8, 9 10
+if szam <= 10 or szam ==100:
+    print(f"A szám kisebb vagy egyenlő mint 10!")
+else:
+    print(f"A szám kisebb vagy egyenlő mint 10!")
